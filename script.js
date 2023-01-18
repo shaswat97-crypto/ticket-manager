f9fd342d69cc3c7935dba46a396ee3b306cbe3bb
shaswat97-crypto <shaswat97@gmail.com>
2023-01-19 00:44:19 +0530

    adding ticket

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 let addbutton = document.querySelector(".addbtn");
+let addbuttonModal = document.querySelector(".add");
 let modal = document.querySelector(".modal");
 let allTickets = document.querySelector(".allTickets");
+let allColors = document.querySelectorAll(".prioritysel");
+let colors = {  low:"green",
+                mid:"yellow", 
+                high:"red"
+            };
+let revcolors = {  green:'low',
+                    yellow:'mid',
+                    red:'high'
+            };
+let currColor = "green";
 let addbtnclick = false;
+let id = 0;
 addbutton.addEventListener("click", (e) => {
     // addbutton.style.display = "flex";
     addbtnclick = !addbtnclick;
@@ -13,27 +25,41 @@ addbutton.addEventListener("click", (e) => {
     }
 });
 
-modal.addEventListener("keydown", (e) => {
-    let key = e.key;
-    if(key === "Shift"){
-        console.log('hi');
-        addTicket();
-        let textArea = document.querySelector(".textarea");
-        modal.style.display = "none";
-        addbtnclick = false;
-        textArea.value = "";
-    }
+//adding to allTickets
+addbuttonModal.addEventListener("click", (e) => {
+    let textArea = document.querySelector(".textarea");
+    // console.log('hi');
+    addTicket(textArea.value, shortid());
+    modal.style.display = "none";
+    addbtnclick = false;
+    textArea.value = "";
+
 })
 
-addTicket = () => {
+addTicket = (text, shortid) => {
     let ticketHolder = document.createElement("div");
     ticketHolder.classList.add("ticket-holder");
+    ticketHolder.setAttribute("id", "id"+id);
     ticketHolder.innerHTML = `
-        <div class="priority-color"></div>
-        <div class="text-area">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio incidunt illo, voluptas illum placeat
-            dignissimos.
-        </div>
+        <div class="priority-color ${revcolors[currColor]}" id="id${id}"></div>
+        <div class="id">ID: ${shortid}</div>
+        <div class="text-area">${text}</div>
     `;
     allTickets.appendChild(ticketHolder);
+    id++;
 }
+
+//selecting priority
+allColors.forEach((color) => {
+    color.addEventListener("click", (e)=>{
+        allColors.forEach((col) => {
+            col.classList.remove("priorityBorder");
+        });
+        color.classList.add("priorityBorder");
+        let currPriority = color.classList[1];
+        // console.log(currPriority, colors[currPriority])
+        currColor = colors[currPriority];
+    })
+})
+
+//
